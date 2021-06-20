1c938e063af1125b5a0bba4390dd04c7127ac6bf
camlemessurier <camlemessurier@gmail.com>
2021-06-20 21:25:24 +1000

    PWA support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,16 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		if ('serviceWorker' in navigator) {
+			window.addEventListener('load', function () {
+				navigator.serviceWorker.register('/sw.js');
+			});
+		}
+	}, []);
 	return (
 		<ChakraProvider resetCSS theme={theme}>
 			<ColorModeProvider
