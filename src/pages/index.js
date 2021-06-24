aa559e1be41036d9a5f6f2d5e9b713fdf1abae6e
camlemessurier <camlemessurier@gmail.com>
2021-06-24 20:57:26 +1000

    added GTM

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,6 +59,7 @@ const Index = () => {
 		>
 			<Head>
 				<title>Cam Le Messurier</title>
+
 				<meta charSet='utf-8' />
 				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
 				<meta
