47d103f62973a174ad72bf5b405eef7222227395
camlemessurier <camlemessurier@gmail.com>
2024-08-20 21:06:22 +1000

    Simple

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,132 +1,57 @@
 import {
 	Box,
-	Flex,
 	HStack,
 	Icon,
 	Link,
-	List,
-	ListIcon,
-	ListItem,
 	Text,
-	useColorMode,
+	VStack
 } from "@chakra-ui/react";
-import { motion } from "framer-motion";
 import { BsFileEarmarkTextFill } from "react-icons/bs";
-import { MdOutlineMailOutline } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 import { SiLinkedin } from "react-icons/si";
-import { TiPhone } from "react-icons/ti";
 import { VscGithubInverted } from "react-icons/vsc";
 
 export const Sidebar = () => {
-	const bgColor = { light: "green.800", dark: "gray.50" };
-
-	const color = { light: "gray.50", dark: "green.800" };
-
-	const variants = {
-		visible: { x: 0 },
-		hidden: { x: -500 },
-	};
-
-	const { colorMode, toggleColorMode } = useColorMode();
 
 	return (
-		<Flex
-			py={{ base: "30px", lg: "5vw" }}
-			w={{ base: "100%", lg: "30vw" }}
-			h={{ base: "auto", lg: "calc(100vh - 4vw)" }}
-			maxWidth={{ base: "", lg: "500px" }}
-			minWidth={"300px"}
-			flexDirection='column'
-			justifyContent='space-between'
-			alignItems='center'
-			bg={bgColor[colorMode]}
-			color={color[colorMode]}
-			borderRadius={20}
-			position={{ base: "initial", lg: "sticky" }}
-			overflow='hidden'
-			top='2vw'
-			shadow='md'
-			p={4}
-		>
+		<VStack pb={10}>
 			<Box>
-				<Flex
-					justifyContent='center'
-					alignItems='center'
-					flexDirection='column'
-					mb='3vw'
-				>
-					<Box display={{ base: "none", lg: "initial" }}>
-						<motion.button
-							whileHover={{ scale: 1.1 }}
-							whileTap={{ scale: 0.9 }}
-						>
-							<Flex
-								borderRadius={4000}
-								bgColor={color[colorMode]}
-								color={bgColor[colorMode]}
-								justifyContent='center'
-								alignItems='center'
-								h='5vw'
-								w='5vw'
-								mb='2vw'
-								onClick={toggleColorMode}
-								cursor='pointer'
-								shadow='md'
-							>
-								<Text fontWeight='600' fontSize='2vw'>
-									CL
-								</Text>
-							</Flex>
-						</motion.button>
-					</Box>
-
-					<Text fontSize={30} fontWeight='600' mb={2} textAlign='center'>
-						Cam Le Messurier
-					</Text>
-					<Text fontSize={18}>Software Engineer</Text>
-
-					<br></br>
-					<Box fontWeight='500'>
-						<List spacing={1}>
-							<ListItem>
-								<Link href='tel:0423886255' isExternal>
-									<ListIcon as={TiPhone} />
-									0423886255
-								</Link>
-							</ListItem>
-
-							<ListItem>
-								<Link href='mailto:camlemessurier@gmail.com' isExternal>
-									<ListIcon as={MdOutlineMailOutline} />
-									camlemessurier@gmail.com
-								</Link>
-							</ListItem>
-						</List>
-					</Box>
-				</Flex>
+				<Text fontSize={70} fontWeight='700' mb={2} textAlign='center'>
+					Cam Le Messurier
+				</Text>
 			</Box>
-
-			<Box mt={7}>
+			<Box>
 				<HStack spacing={30}>
 					<Link
 						href='https://github.com/camlemessurier'
 						aria-label='Github'
 						isExternal
 					>
-						<Icon as={VscGithubInverted} w={10} h={10} />
+						<Icon as={VscGithubInverted} w={12} h={12} />
 					</Link>
 					<Link
 						href='https://www.linkedin.com/in/camlemessurier/'
 						aria-label='LinkedIn'
 						isExternal
 					>
-						<Icon as={SiLinkedin} w={9} h={9} />
+						<Icon as={SiLinkedin} w={12} h={12} />
+					</Link>
+					<Link
+						href='mailto:camlemessurier@gmail.com'
+						aria-label='Email'
+						isExternal
+					>
+						<Icon as={MdEmail} w={55} h={55} />
 					</Link>
 					<Link href='/Resume.pdf' aria-label='Resume' isExternal>
-						<Icon as={BsFileEarmarkTextFill} w={9} h={9} />
+						<Icon as={BsFileEarmarkTextFill} w={12} h={12} />
 					</Link>
 				</HStack>
 			</Box>
-		</Flex>
+
+		</VStack>
+
+
+
 	);
 };
--- src/content/resumeContent.ts
+++ src/content/resumeContent.ts
@@ -1,18 +1,42 @@
 export const resumeContent = {
-	heading: "Hello, welcome to my resume",
+	heading: "Hi, welcome to my resume",
 	mainText:
-		"Experienced full stack software engineer focused on building high quality digital solutions. Strong advocate for process improvement and continually seeks out opportunities to improve development workflows, share knowledge, and support the team in any way I can. Loves a challenge and the odd rogue side mission.",
+		"Over the past few years, I've had the opportunity to work across a wide range of industries and teams; from banks to startups to consultancy firms. Over this time, I've been able to help on a lot of interesting projects, worked with some exceptional teams and met some fantastic people along the way. I am always eager to contribute and help the team deliver top-quality software",
 	employment: [
 		{
-			title: "Software Engineer, Greater Bank, Newcastle",
-			timeText: "April 2022 - PRESENT",
+			title: "Software Engineer, Digizoo Services",
+			timeText: "October 2023 - PRESENT",
 			blurb:
-				"Worked within cross-disciplinary team to develop a digital banking solution for over 300,000 customers; 15+ backend micro-services, 3+ frontends, and 5+ teams. Responsible for end-to-end feature delivery, specialising in web, backend, and CI/CD pipelines.",
+				"As a consultant for Digizoo, I work primarily at ING where I'm helping build, design and release real-time, high volume data pipelines to power critical components within ING's current and future systems. This project revolves around delivering data where it was needed fast and reliable - involving a lot of complex technical, operational and organisational challenges for us and the wider team to tackle.",
 			acheivements: [
-				"Planned, implemented, and communicated advanced multi-repo release and regression testing process across the entire digital banking solution",
-				"Pioneered shared CI/CD configuration across micro-services to enhance release, testing, quality, and compliance processes",
-				"Actioned automated dependency management solution for 30+ repositories to meet strict security requirements",
-				"Heavily involved in performance testing and optimisation of the system, leading to considerable increases in application stability and a significant reduction of API response times (over 50%)",
+				"Design and implementation of data streaming systems - focusing on ensuring accuracy, validity and performance at high volumnes",
+				"Application of domain-driven and event-driven architecture principles to ensure proper decoupling of data and resiliency in the face of issues",
+				"Helping perfect and productionise various event driven components - from pipelines and infrastructure to observalibility and testing",
+			],
+			technologies: [
+				"Kafka",
+				"Kafka Streams",
+				"Kafka Connect",
+				"SQL Server",
+				"Spring",
+				"Java",
+				"Graphana",
+				"Prometheus",
+				"OTEL",
+				"Azure",
+				"Unix"
+			],
+		},
+		{
+			title: "Software Engineer, Greater Bank, Newcastle",
+			timeText: "April 2022 - October 2023",
+			blurb:
+				"As part of Greater Bank's digital transformation project, I worked with huge team to help build and deliver a modern, cloud-first digital banking platform from scratch; new front ends, new backends, new infrastructure and new processes.",
+			acheivements: [
+				"Worked on multi-repo release and regression strategy across the entire digital stack",
+				"Helped build and deploy shared CI/CD configuration across micro-services to standardise and enforce release, testing, quality, and compliance processes",
+				"Implemented automated dependency management solution for 30+ repositories to meet strict security requirements",
+				"Conducted performance testing and worked on identifying and fix optimisation bottlenecks within the system, leading to considerable increases in application stability and significant reduction of API response times",
 			],
 			technologies: [
 				"Kubernetes",
@@ -32,13 +56,12 @@ export const resumeContent = {
 			title: "Software Engineer, BodyMindLife Online, Newcastle",
 			timeText: "OCTOBER 2021 - April 2022",
 			blurb:
-				"Worked as full stack developer within startup team to uplift and improve online yoga streaming platform. Primarily responsible for front end web development and testing automation.",
+				"Working within a dynamic startup environment, I helped build and grow a new online yoga streaming platform.",
 			acheivements: [
-				"Helped collaborate with business on feature backlog to prioritise and design solutions",
-				"Helped automate and integrate end-to-end testing to CI/CD pipelines",
-				"Developed and deployed small authentication micro-service to connect multiple IAM providers",
-				"SEO uplift of web app",
-				"Significant front end UI/UX revamp",
+				"Involved in all stages of feature development from inception to deployment",
+				"Lots of automation and integration for end-to-end testing by CI/CD pipelines",
+				"SEO and accessibility audit and uplift",
+				"Responsible for authentication micro-service gateway to connect multiple IAM providers",
 			],
 			technologies: [
 				"NextJs",
@@ -58,11 +81,11 @@ export const resumeContent = {
 			title: "Junior Software Engineer, Newton Green Technologies, Newcastle",
 			timeText: "FEBRUARY 2021 - SEPTEMBER 2021",
 			blurb:
-				"Worked as a full stack developer on a variety of projects for clients.",
+				"Worked on a variety projects for clients",
 			acheivements: [
-				"Full stack development on communication platform for startup project in aged care",
-				"Full stack development on well established community pest management tracker",
-				"Front end development for various cancer research agencies",
+				"Development for a real-time communication startup project for aged care facilities",
+				"Worked on on well established community pest management tracker",
+				"Contributed to front-end development for various cancer research agencies",
 			],
 			technologies: [
 				"Laravel",
@@ -78,7 +101,7 @@ export const resumeContent = {
 			title: "Intern, Userlot, Newcastle",
 			timeText: "FEBRUARY 2021 - SEPTEMBER 2021",
 			blurb:
-				"Built an embeddable third part widget to collect and analyse surveys from customers for local tech startup, as a part of UON's Work Integrated Learning (WIL) project",
+				"As a part of UON's Work Integrated Learning (WIL) project, I built an embeddable third part widget to collect surveys from customers and capture data in dashboard for local tech startup",
 			technologies: [
 				"React",
 				"Apollo Client",
@@ -94,7 +117,7 @@ export const resumeContent = {
 				"Junior Developer / Acoustic Technician, Global Acoustics, Newcastle",
 			timeText: "JULY 2019 - FEBRUARY 2021",
 			blurb:
-				"Worked as on the ground tech, but also part time as developer. Solely responsible for delivery of a full stack safety management tracker used by techs everyday",
+				"Worked as a part time developer / part-time technican. Solely responsible for delivery of a full stack safety management tracker used by on the ground teams everyday to log safetly and compliance paperwork",
 			technologies: [
 				"Next",
 				"Express",
@@ -114,10 +137,10 @@ export const resumeContent = {
 			title:
 				"Bachelor of Computer Science / Mathematics, University of Newcastle",
 			blurb:
-				"Courses included advanced databases, algorithms, system and network security, web development, predictive analytics, operating systems and human computer interaction, linear algebra, statistical inference, machine learning, systems thinking",
+				"Studied topics including artificial intelligence, systems thinking, data science,  machine learning, databases, algorithms, system and network security, web development, predictive analytics, operating systems and human computer interaction, linear algebra, statistical inference ",
 			acheivements: [
-				"Dr Robert M Sheahan Memorial Scholarship",
-				"Qualified with Distinction",
+				"Recipient of the Dr Robert M Sheahan Memorial Scholarship",
+				"Graduated with Distinction",
 			],
 		},
 	],
